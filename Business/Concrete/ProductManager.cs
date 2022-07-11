ecfdd9b042e4ae11ddc652aa111ab8486528bbed
= <=>
2022-07-11 22:43:37 +0300

    Add business methods

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -19,5 +19,15 @@ namespace Business.Concrete
         {
             return _productDal.GetAll();
         }
+
+        public List<Product> GetAllByCategoryId(int categoryId)
+        {
+            return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
     }
 }
