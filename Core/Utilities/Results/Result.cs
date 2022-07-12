d3cbb8432ab12bc04ba1a4a248ddb29f0abdb666
= <=>
2022-07-12 22:27:05 +0300

    Add notification management for utilities

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        // Getter is readonly. Readonly objects only can be set in the constructor. 
+        public Result(bool success, string message) : this(success)
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
