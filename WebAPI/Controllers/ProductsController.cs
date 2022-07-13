35a5f9aaceef7461c892bcbd99a1b73336db61c2
= <=>
2022-07-13 16:10:57 +0300

    Add ProductController

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Entities.DTOs.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        } 
+
+        [HttpGet]
+        public IDataResult<List<ProductDetailDto>> Get()
+        {
+            var result = _productService.GetProductDetails();
+            return new SuccessDataResult<List<ProductDetailDto>>(result.Data, result.Message);
+        }
+    }
+}
