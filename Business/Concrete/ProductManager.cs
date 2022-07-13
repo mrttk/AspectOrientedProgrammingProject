8a6c178b99cd6e21c70c0865bcac32178b330be8
= <=>
2022-07-13 22:01:41 +0300

    Add validation tool for core layer

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConserns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -22,12 +23,7 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
-            var validationContext = new ValidationContext<Product>(product);
-            ProductValidator productValidator = new ProductValidator();
-            var result = productValidator.Validate(validationContext);
-
-            if (!result.IsValid)
-                throw new ValidationException(result.Errors);
+            ValidationTool.Validate(new ProductValidator(), product);
 
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
---
+++ Core/CrossCuttingConserns/Validation/ValidationTool.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConserns.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator,object entity)
+        {
+            var validationContext = new ValidationContext<object>(entity);
+            var result = validator.Validate(validationContext);
+
+            if (!result.IsValid)
+                throw new ValidationException(result.Errors);
+        }
+    }
+}
